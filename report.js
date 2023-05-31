3c4edd7ac38b34ffb04d29b14ef8599ebb21817d
Musfika Jahan <musfika.jahan@bracits.com>
2023-05-31 18:27:35 +0600

    upload

---
+++ report.js
@@ -0,0 +1,15 @@
+const newman = require('newman');
+ 
+newman.run({
+    collection: require('./collection/dmoney_rest_API_assignment.json'),
+    reporters: 'htmlextra',
+    iterationCount: 1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        }
+    }
+}, function (err) {
+    if (err) { throw err; }
+    console.log('collection run complete!');
+});
